466c0b7f6d33b6f5caff8bd563d116ed08ad0c6e
Karolina Sokhar <coxapkarolinka@mail.ru>
2023-06-20 20:03:39 +0300

    adding new elements

--- 014 script.js
+++ 014 script.js
@@ -48,3 +48,60 @@ switch (num) {
         break;
 
 }
+
+
+
+// Логические операторы
+
+// const hamburger = 5;
+// const fries = 0;
+
+// if (hamburger && fries) {
+//     console.log("I'm full!");
+// }
+
+
+
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 0;
+
+// console.log(hamburger === 3 && cola && fries); // вернет 0
+
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && 'hgvshdbcxj');
+
+
+// if (hamburger === 3 && cola === 1 && fries) {
+//     console.log("Everyone is full!");
+// } else {
+//     console.log('Were leaving!');
+// }
+
+
+
+
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+
+
+if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+    console.log("Everyone is full!");
+} else {
+    console.log('Were leaving!');
+}
+
+console.log(hamburger === 3 && cola === 2);
+
+
+let johnReport, alexReport, samReport, mariaReport = 'done';
+
+console.log(johnReport || alexReport || samReport || mariaReport);
+
+
+
+console.log(!0);
