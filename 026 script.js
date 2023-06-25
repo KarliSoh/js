51a2518dab82609dec01bd71a53cab9bcbf2a020
Karolina Sokhar <coxapkarolinka@mail.ru>
2023-06-25 16:08:16 +0300

    new file 026 - add new elements

---
+++ 026 script.js
@@ -0,0 +1,47 @@
+"use sctrict";
+
+const options = {
+    name: 'test',
+    wisth: 1024,
+    hight: 2014,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function () {
+        console.log('Test');
+    }
+};
+
+options.makeTest();
+
+const {
+    border,
+    bg
+} = options.colors; //деструктуризация объекта
+console.log(border);
+
+
+// console.log(Object.keys(options).length); 
+
+// console.log(options['colors']['border']);
+
+// console.log(options.name);
+
+// delete options.name;
+
+// console.log(options);
+// let counter = 0;
+
+// for (let key in options) {
+//     if (typeof (options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         counter++;
+//     }
+
+// }
+// console.log(counter);
