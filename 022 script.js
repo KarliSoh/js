ee58b1cf820bc276c6131fbb2c875db0704c4503
Karolina Sokhar <coxapkarolinka@mail.ru>
2023-06-25 13:35:51 +0300

    new element - 022

--- 022 script.js
+++ 022 script.js
@@ -1,3 +1,35 @@
 "use strict";
 
-const str = ''
+const str = 'test';
+const str1 = 'teSt';
+const arr = [1, 2, 4];
+
+
+// console.log(str[2] = 'd'); //d
+
+console.log(str.toUpperCase()); //TEST
+console.log(str1.toLocaleLowerCase()); //test
+console.log(str); // 1, 2, 4
+
+const fruit = 'Some fruit';
+
+
+console.log(fruit.indexOf('fruit'));
+
+
+const logg = 'Hello world';
+
+// console.log(logg.slice(6, 11));
+
+// console.log(logg.substring(6, 11));
+
+console.log(logg.substr(6, 5));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = '12.2px';
+
+console.log(parseInt(test)); //12
+
+console.log(parseFloat(test)); //12.2
