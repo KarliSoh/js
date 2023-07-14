a13933dfb1020db3f1d1dfc376ff7f8dce28ec27
Karolina Sokhar <coxapkarolinka@mail.ru>
2023-07-14 22:24:36 +0300

    new elem

--- операторы нулевого слияния/script.js
+++ операторы нулевого слияния/script.js
@@ -2,12 +2,18 @@
 
 const box = document.querySelector('.box');
 
-const newHeight = 100;
+const newHeight = 0;
 const newWidth = 400;
 
 function changeParams(elem, h, w) {
-    elem.style.height = `${h}px`;
-    elem.style.width = `${w}px`;
+    elem.style.height = `${h || 200}px`; // elem.style.height = `${h ?? 200}px`;
+    elem.style.width = `${w || 200}px`; //elem.style.width = `${w ?? 200}px`;
+    // elem.innerHTML = (h ? ? 200) * (w ? ? 200);
 }
 
 changeParams(box, newHeight, newWidth);
+
+let userName;
+let userKey;
+
+console.log(userName && userKey && 'User');
