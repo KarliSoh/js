e71f3ed23095e6e1a0c625d49fcf34e69d6f4dae
Karolina Sokhar <coxapkarolinka@mail.ru>
2023-07-22 00:05:39 +0300

    new file 050

---
+++ 050 script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// const bigint = 654789228374637202983254851254152n;
+
+// const sameBigInt = BigInt(654789228374637202983254851254152);
+
+// console.log(2n === 2);
+
+let bigint = 1n;
+let num = 2;
+
+console.log(bigint + BigInt(num));
+console.log(Number(bigint) + num);
